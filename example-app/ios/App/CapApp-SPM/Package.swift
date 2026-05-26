6e354961d27cc30b7ba6bf4f5bae9b9126a7e6ef
Martin Donadieu <martindonadieu@gmail.com>
2026-05-26 14:23:48 +0200

    fix SwiftPM product name for Capacitor v8

--- example-app/ios/App/CapApp-SPM/Package.swift
+++ example-app/ios/App/CapApp-SPM/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
         .package(name: "CapacitorCamera", path: "../../../node_modules/.bun/@capacitor+camera@8.0.0+15e98482558ccfe6/node_modules/@capacitor/camera"),
         .package(name: "CapacitorSplashScreen", path: "../../../node_modules/.bun/@capacitor+splash-screen@8.0.0+15e98482558ccfe6/node_modules/@capacitor/splash-screen"),
-        .package(name: "CapgoHomeIndicator", path: "../../../node_modules/.bun/@capgo+capacitor-home-indicator@file+../node_modules/@capgo/capacitor-home-indicator")
+        .package(name: "CapgoCapacitorHomeIndicator", path: "../../../node_modules/.bun/@capgo+capacitor-home-indicator@file+../node_modules/@capgo/capacitor-home-indicator")
     ],
     targets: [
         .target(
@@ -24,7 +24,7 @@ let package = Package(
                 .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "CapacitorCamera", package: "CapacitorCamera"),
                 .product(name: "CapacitorSplashScreen", package: "CapacitorSplashScreen"),
-                .product(name: "CapgoHomeIndicator", package: "CapgoHomeIndicator")
+                .product(name: "CapgoCapacitorHomeIndicator", package: "CapgoCapacitorHomeIndicator")
             ]
         )
     ]
