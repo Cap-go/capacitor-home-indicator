4a3fd4742761823d75b70ea0db8ed56970a119ac
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 13:13:37 +0200

    style: format capgo example rollout

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -3,23 +3,23 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import pkg from './package.json';
 
 const config: CapacitorConfig = {
-  "appId": "app.capgo.home.indicator.example",
-  "appName": "Home Indicator Example",
-  "bundledWebRuntime": false,
-  "webDir": "dist",
-  "plugins": {
-    "SplashScreen": {
-      "launchShowDuration": 0
+  appId: 'app.capgo.home.indicator.example',
+  appName: 'Home Indicator Example',
+  bundledWebRuntime: false,
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
     },
-    "CapacitorUpdater": {
-      "appId": "app.capgo.home.indicator.example",
-      "autoUpdate": true,
-      "autoSplashscreen": true,
-      "directUpdate": "always",
-      "defaultChannel": "production",
-      "version": pkg.version
-    }
-  }
+    CapacitorUpdater: {
+      appId: 'app.capgo.home.indicator.example',
+      autoUpdate: true,
+      autoSplashscreen: true,
+      directUpdate: 'always',
+      defaultChannel: 'production',
+      version: pkg.version,
+    },
+  },
 };
 
 export default config;
